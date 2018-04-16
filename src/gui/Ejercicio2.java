a8bcca9ca5ae143a04dc520bc52cdb08c42f94c8
Jesús <jesusruiver@jesusruiver.com>
2018-04-16 13:49:28 +0200

    Primeras prubeas Ejercicio 2 y 3

--- src/gui/Ejercicio2.java
+++ src/gui/Ejercicio2.java
@@ -2,14 +2,25 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import bbdd.Conexion;
+
+import javax.swing.JComboBox;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+
 public class Ejercicio2 extends JFrame {
 
 	private JPanel contentPane;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+	
+	private Conexion miConexion = new Conexion();
 
 	/**
 	 * Launch the application.
@@ -31,12 +42,55 @@ public class Ejercicio2 extends JFrame {
 	 * Create the frame.
 	 */
 	public Ejercicio2() {
+		
+		miConexion.conectar();
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 365, 403);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JComboBox cboxTiendas = new JComboBox();
+		cboxTiendas.setBounds(42, 28, 272, 23);
+		contentPane.add(cboxTiendas);
+		
+		JRadioButton rbtnVentas = new JRadioButton("Ventas");
+		buttonGroup.add(rbtnVentas);
+		rbtnVentas.setBounds(109, 62, 109, 23);
+		contentPane.add(rbtnVentas);
+		
+		JRadioButton rbtnPedidos = new JRadioButton("Pedidos");
+		buttonGroup.add(rbtnPedidos);
+		rbtnPedidos.setBounds(109, 95, 109, 23);
+		contentPane.add(rbtnPedidos);
+		
+		JButton btnExportarBinarioSecuencial = new JButton("Exportar Fichero Binario");
+		btnExportarBinarioSecuencial.setBounds(133, 148, 181, 23);
+		contentPane.add(btnExportarBinarioSecuencial);
+		
+		JButton btnExportarXML = new JButton("Exportar Fichero XML");
+		btnExportarXML.setBounds(133, 193, 181, 23);
+		contentPane.add(btnExportarXML);
+		
+		JButton btnImportarBinarioSecuencial = new JButton("Importar Fichero Binario");
+		btnImportarBinarioSecuencial.setBounds(133, 240, 181, 23);
+		contentPane.add(btnImportarBinarioSecuencial);
+		
+		JButton btnImportarFicheroXML = new JButton("Importar Fichero XML");
+		btnImportarFicheroXML.setBounds(133, 292, 181, 23);
+		contentPane.add(btnImportarFicheroXML);
+		
+		
+		ArrayList<String> lista = new ArrayList<String>();
+		
+		lista = miConexion.rellenaComboBox();
+		
+		for (int i = 0; i < lista.size(); i++){
+			cboxTiendas.addItem(lista.get(i));
+		}
+		
+		
 	}
-
 }
--- src/gui/Ejercicio3.java
+++ src/gui/Ejercicio3.java
@@ -2,14 +2,30 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import bbdd.Conexion;
+
+import javax.swing.JScrollPane;
+import javax.swing.JList;
+import javax.swing.JComboBox;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+
 public class Ejercicio3 extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	
+	private Conexion miConexion = new Conexion();
 
 	/**
 	 * Launch the application.
@@ -31,12 +47,76 @@ public class Ejercicio3 extends JFrame {
 	 * Create the frame.
 	 */
 	public Ejercicio3() {
+		
+		miConexion.conectar();
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 536, 410);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(26, 34, 230, 308);
+		contentPane.add(scrollPane);
+		
+		JList listTiendas = new JList();
+		scrollPane.setViewportView(listTiendas);
+		
+		JComboBox cboxArticulos = new JComboBox();
+		cboxArticulos.setBounds(279, 31, 199, 20);
+		contentPane.add(cboxArticulos);
+		
+		JSpinner spinner = new JSpinner();
+		spinner.setBounds(371, 88, 107, 20);
+		contentPane.add(spinner);
+		
+		JSpinner spinner_1 = new JSpinner();
+		spinner_1.setBounds(371, 150, 107, 20);
+		contentPane.add(spinner_1);
+		
+		textField = new JTextField();
+		textField.setBounds(371, 220, 107, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(371, 270, 107, 20);
+		contentPane.add(textField_1);
+		
+		JLabel lbPeso = new JLabel("Peso");
+		lbPeso.setBounds(279, 91, 46, 14);
+		contentPane.add(lbPeso);
+		
+		JLabel lbunidades = new JLabel("Unidades");
+		lbunidades.setBounds(279, 153, 46, 14);
+		contentPane.add(lbunidades);
+		
+		JLabel lbFecha = new JLabel("Fecha");
+		lbFecha.setBounds(279, 223, 46, 14);
+		contentPane.add(lbFecha);
+		
+		JLabel lbCategoria = new JLabel("Categoria");
+		lbCategoria.setBounds(277, 273, 64, 14);
+		contentPane.add(lbCategoria);
+		
+		JButton btnInsertarArticulo = new JButton("Insertar");
+		btnInsertarArticulo.setBounds(389, 324, 89, 23);
+		contentPane.add(btnInsertarArticulo);
+		
+		DefaultListModel<String> modeloLista = new DefaultListModel<String>();
+		
+		ArrayList<String> datos = new ArrayList<String>();
+		
+		datos = miConexion.rellenaComboBox();
+		
+		for (int i = 0; i<datos.size();i++){
+			modeloLista.addElement(datos.get(i));
+		}
+		
+		listTiendas.setModel(modeloLista);
+		
 	}
-
 }
